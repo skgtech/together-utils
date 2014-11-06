c371d8c27804d0635783a6d9cf7384f5d1c37261
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2014-11-06 12:55:01 +0200

    Add the PDF collate utils of together I & II

---
+++ together/script-to-collate-pdfs-on-osx.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py --output all-sans-moz.pdf --verbose MAKEDIY\ -\ Παρουσίαση\ skg\ tech.pdf Arduino_SKGTech_low_res.pdf Together2014-thessrb.pdf Java\ Meetup\ Presentation.pdf SKGTech\ iOS.pdf behance_presentation.pdf presentation_CP_TOGETHER.pdf Agile\ Meetup\ Presentation.pdf Steki_socialmedia_presentation.pdf open_thessaloniki_together.pdf open_coffee_thessaloniki_together.pdf
---
+++ wearetogetherII/collate-pdfs-osx.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py --output master.pdf --verbose \
+SKGTech\ Presentation.pdf \
+ΜΑΚΕ_Together.pdf \
+ArduinoGroupTogether.pdf \
+thessrb_2nd_together_event_presentation.pdf \
+Java\ Meetup\ Presentation.pdf \
+SKGnodejs.pdf \
+python-presentation.pdf \
+We\ are\ together\ -\ iOSDevsGr.pdf \
+drupal@2nd-we-are-together.pdf \
+dotnet.pdf \
+fido-presentation.pdf \
+break.pdf \
+whatisMozillaTogether2.pdf \
+behance_together_presentation_7.11.pdf \
+keftedes.pdf \
+agile_weAreTogether.pdf \
+Steki_socialmedia_presentation.pdf \
+SKG\ Startupers.pdf \
+StartupLive.pdf \
+OpenKnowledgeGreece.pdf \
+WiFi\ communities\ -\ http---skgtech.io-together\ Event.pdf \
+open_thess.pdf \
+Steli\ Efti.pdf \
+octhess_together_v2.pdf \
+Αγροτική\ Επιχειρηματικότητα.pdf \
+Παρουσίαση\ ΟΦΑ.pdf \
+logo.pdf
